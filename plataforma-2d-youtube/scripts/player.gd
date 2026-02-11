7db5bd3150d0a12b6e4c2efa9864258429b46583
caique <caique405@gmail.com>
2026-02-11 17:41:04 -0300

    Implementando double jump

--- plataforma-2d-youtube/scripts/player.gd
+++ plataforma-2d-youtube/scripts/player.gd
@@ -3,17 +3,21 @@ extends CharacterBody2D
 enum PlayerState {
 	idle,
 	walk,
-	jump
+	jump,
+	duck
 }
 
 # Faz uma referencia ao nó da arvore que se chama AnimatedSprite2D 
 # Atribuindo a uma variável chamada anim do tipo AnimetadesSprite2D
 @onready var  anim: AnimatedSprite2D = $AnimatedSprite2D
-
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+var direction: float = 0.0
 const SPEED = 70.0
 const JUMP_VELOCITY = -300
-
 var status: PlayerState
+var jump_count: int = 0
+@export var max_jump_count: int = 2
+
 
 func _ready() -> void:
 	go_to_idle_state()
@@ -31,6 +35,8 @@ func _physics_process(delta: float) -> void:
 			walk_state()
 		PlayerState.jump:
 			jump_state()
+		PlayerState.duck:
+			duck_state()
 	
 	move_and_slide()
 
@@ -46,6 +52,13 @@ func go_to_jump_state():
 	status = PlayerState.jump
 	anim.play("jump")
 	velocity.y = JUMP_VELOCITY
+	jump_count += 1
+	
+func go_to_duck_state():
+	status = PlayerState.duck
+	collision_shape_2d.shape.height = 10.0
+	collision_shape_2d.position.y = 3
+	anim.play("duck")
 
 func idle_state():
 	move()
@@ -54,6 +67,10 @@ func idle_state():
 		return
 	if Input.is_action_just_pressed("jump"):
 		go_to_jump_state()
+		return
+		
+	if Input.is_action_pressed("duck"):
+		go_to_duck_state()
 		return
 	
 func walk_state():
@@ -67,19 +84,38 @@ func walk_state():
 
 func jump_state():
 	move()
+	
+	if Input.is_action_just_pressed("jump") && jump_count < max_jump_count:
+		go_to_jump_state()
+	
 	if is_on_floor():
+		jump_count = 0
 		if velocity.x == 0:
 			go_to_idle_state()
 		else:
 			go_to_walk_state()
 		return
 
+func duck_state():
+	update_direction()
+	if Input.is_action_just_released("duck"):
+		exit_from_duck_state()
+		go_to_idle_state()
+		return
+		
+func exit_from_duck_state():
+	collision_shape_2d.shape.height = 16.0
+	collision_shape_2d.position.y = 0
+
 func move():
-	var direction := Input.get_axis("left", "right") # retorna -1 ou 1 
+	update_direction()
 	if direction: # se direction != 0 atualizar posicao (para esquerda ou direita)
 		velocity.x = direction * SPEED # atualiza a posição em função da var SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED) # para o player devagar (não bruscamente)
+
+func update_direction():
+	direction = Input.get_axis("left", "right") # retorna -1 ou 1 
 	# flipando o sprite
 	if direction < 0:
 		anim.flip_h = true
