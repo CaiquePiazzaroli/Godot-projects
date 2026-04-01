7c869033527a729ce99d803e3a63cdf79f6b5458
caique <caique405@gmail.com>
2026-04-01 18:52:03 -0300

    Implementando ataque do inimigo

--- [end-game-beat-and-up-project]/scripts/player.gd
+++ [end-game-beat-and-up-project]/scripts/player.gd
@@ -1,73 +1,61 @@
 extends CharacterBody2D
 
-
-@export var acceleration = 400
-@export var max_speed = 100
-@export var jump_velocity = -300
-@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
-
-# direção do player
-var direction: float = 0.0
-
-# Possíveis estados do player e variavel state inicial
 enum PlayerState {
 	idle,
 	walk,
-	jump
+	jump,
+	death
 }
+
+@export var life: int = 100
+@export var acceleration = 400
+@export var max_speed = 50
+@export var jump_velocity = -180
+@onready var player_animation: AnimatedSprite2D = $PlayerAnimation
+@onready var player_collision: CollisionShape2D = $PlayerCollision
+
+var direction: float = 0.0
 var state : PlayerState
 
-# Movimenta o player quando pressionado o botao
-func move(delta : float) -> void:
-	update_direction() # Atualiza direção 
-	if direction: # acelera até a velocidade 100
-		velocity.x = move_toward(velocity.x, max_speed * direction, acceleration * delta)
-
-	else: # Desacelera até a velocidade 0
-		velocity.x = move_toward(velocity.x, 0, acceleration * delta) #zera a velocidade em x
-
-# Atualiza a var direction e flipa o sprite
-func update_direction():
-	direction = Input.get_axis("left", "right")
-	if direction > 0:
-		anim.flip_h = false
-	elif direction < 0:
-		anim.flip_h = true
-
-# Inicia o player com estado idle
 func _ready() -> void:
 	go_to_idle_state()
 
-func _input(_event: InputEvent) -> void:
-	if Input.is_action_just_pressed("quit"):
-		get_tree().quit()
-
 func _physics_process(delta: float) -> void:
-
 	match state:
 		PlayerState.idle:
 			idle_state(delta)
 		PlayerState.walk:
-			walk_state(delta)
+			walk_state(delta)	
 		PlayerState.jump:
 			jump_state(delta)
-	
+		PlayerState.death:
+			death_state(delta)
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+		return
+	if Input.is_action_just_pressed("death"):
+		go_to_death_state()
 
 func go_to_idle_state():
 	state = PlayerState.idle #altera estado para idle
-	anim.play("idle") # muda anim para idle
-	
+	player_animation.play("idle") # muda anim para idle
 
 func go_to_walk_state():
 	state = PlayerState.walk # altera estado para walk
-	anim.play("walk") # muda anim para walk
-	
+	player_animation.play("walk") # muda anim para walk
 
 func go_to_jump_state():
 	state = PlayerState.jump #altera estado para jump
-	anim.play("jump") # Muda animação
+	player_animation.play("jump") # Muda animação
 	velocity.y = jump_velocity # atribui velocidade de pular
-	
+
+func go_to_death_state():
+	state = PlayerState.death
+	player_collision.disabled = true
+	player_animation.play("death")
+	velocity.x = 0
 
 func idle_state(delta:float) -> void:
 	apply_gravity(delta) # Permite ação da gravidade
@@ -101,6 +89,8 @@ func jump_state(delta: float):
 		go_to_idle_state()
 		return
 
+func death_state(_delta:float):
+	pass
 
 # Gravidade - Atribui uma velocidade positiva no eixo Y fazendo o player cair
 func apply_gravity(delta: float) -> void:
@@ -109,3 +99,19 @@ func apply_gravity(delta: float) -> void:
 	
 	## Necessário para atualizar velocity
 	move_and_slide()
+
+# Movimenta o player quando pressionado o botao
+func move(delta : float) -> void:
+	update_direction() # Atualiza direção 
+	if direction: # acelera até a velocidade 50
+		velocity.x = move_toward(velocity.x, max_speed * direction, acceleration * delta)
+	else: # Desacelera até a velocidade 0
+		velocity.x = move_toward(velocity.x, 0, acceleration * delta) #zera a velocidade em x
+
+# Atualiza a var direction e flipa o sprite
+func update_direction():
+	direction = Input.get_axis("left", "right")
+	if direction > 0:
+		player_animation.flip_h = false
+	elif direction < 0:
+		player_animation.flip_h = true
--- [end-game-beat-and-up-project]/scripts/zombie.gd
+++ [end-game-beat-and-up-project]/scripts/zombie.gd
@@ -1,22 +1,108 @@
 extends CharacterBody2D
 
-@onready var anim: AnimatedSprite2D = $AnimatedSprite2D
+@onready var zombie_animation: AnimatedSprite2D = $ZombieAnimation
+@onready var zombie_walk_timer: Timer = $ZombieWalkTimer
+@onready var zombie_attack: RayCast2D = $ZombieAttackRayCast2D
+@export var zombie_velocity: int = -10
+@onready var zombie_hit_area_collision: CollisionShape2D = $ZombieHitArea/ZombieHitAreaCollision
 
+var direction: float = 0.0
+var state: ZombieState
+
+enum ZombieState {
+	idle,
+	walk,
+	attack,
+}
+
+func _ready() -> void:
+	go_to_walk_state()
+	zombie_walk_timer.start()
 
 func _physics_process(delta: float) -> void:
+	match state:
+		ZombieState.idle:
+			idle_state(delta)
+		ZombieState.walk:
+			walk_state(delta)
+		ZombieState.attack:
+			attack_state(delta)
+
+func go_to_idle_state() -> void:
+	zombie_animation.play("zombie_idle")
+	state = ZombieState.idle
+
+func go_to_walk_state() -> void:
+	zombie_animation.play("zombie_walk")
+	state = ZombieState.walk
+
+func go_to_attack_state() -> void: 
+	zombie_animation.play("attack")
+	state = ZombieState.attack
+
+
+func idle_state(delta: float):
+	apply_gravity(delta)
+	apply_velocity()
+	
+	if velocity.x != 0:
+		go_to_walk_state()
+		return
+
+func walk_state(delta: float):
+	apply_gravity(delta)
+	apply_velocity()
+	
+	if velocity.x == 0:
+		go_to_idle_state()
+		return
+		
+	if zombie_attack.is_colliding():
+		go_to_attack_state()
+		return
+
+func attack_state(delta:float):
+	apply_gravity(delta)
+	apply_velocity()
+	
+	if zombie_animation.frame == 1: # Ativa colisão do hit
+		zombie_hit_area_collision.disabled = false
+	
+	if !zombie_animation.is_playing():
+		zombie_hit_area_collision.disabled = true #Desativa a colisão do hit
+		go_to_walk_state()
+		return
+
+func _on_zombie_walk_timer_timeout() -> void:
+	set_opposite_state()
+
+func set_opposite_state() -> void:
+	if state == ZombieState.walk:
+		go_to_idle_state()
+	elif state == ZombieState.idle: 
+		go_to_walk_state()
+		invertZombieDirection()
+
+func apply_gravity(delta: float) -> void:
 	if !is_on_floor():
 		velocity += get_gravity() * delta
-		
-	if velocity.x != 0:
-		anim.play("zombie_walk")
-		if velocity.x > 0:
-			anim.flip_h = true
-		else:
-			anim.flip_h = false
-	else:
-		anim.play("zombie_idle")
-	
-	velocity.x = 10
-	
-	
+
+func apply_velocity():
+	match state:
+		ZombieState.idle:
+			velocity.x = 0
+		ZombieState.walk:
+			velocity.x = zombie_velocity
+		ZombieState.attack:
+			velocity.x = 0
 	move_and_slide()
+
+func invertZombieDirection():
+	zombie_velocity *= -1
+	scale.x *= -1
+
+func _on_hit_area_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		var player: CharacterBody2D = body
+		player.call_deferred("go_to_death_state") # Muda o player para death
+		player.collision_layer = 0 # Altera a camada de colisão para nao colidir mais
